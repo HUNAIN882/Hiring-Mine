872a21dcc89a76f5acc9ac2e3a422093c0daa42e
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-11-01 17:53:19 -0700

    Add new paragraph

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
-        body.style.color = '#313131';
+        body.style.color = 'black';
         body.style.transition = '2s';
     }else{
-        body.style.background = '#313131';
+        body.style.background = 'black';
         body.style.color = 'white';
         body.style.transition = '2s';
     }
